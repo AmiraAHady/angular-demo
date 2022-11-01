d5e1f97f50557dc0b8e2f726aea04294d8ae9d0c
AmiraAHady <amirasgithub@gmail.com>
2022-11-01 10:16:20 +0200

    start functionality of movie listing

---
+++ src/app/movieslist/movieslist.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../movies.service';
+
+@Component({
+  selector: 'app-movieslist',
+  templateUrl: './movieslist.component.html',
+  styleUrls: ['./movieslist.component.css']
+})
+export class MovieslistComponent implements OnInit {
+  isDiabled:boolean =true 
+  childmsg:string='';
+  pageSize:number=20
+  length!:number;
+  currentPage:number=1
+ private _searchValue:string='' 
+ moviesData:any[]=[]
+ filteredMovies:any[]=this.moviesData; 
+  constructor(private movsService :MoviesService) { }
+
+  ngOnInit(): void {
+    console.log('retrive movies from api');
+    
+  }
+  get searchValue():string{
+    return this._searchValue;
+   }
+ 
+   set searchValue(value:string){ 
+     this._searchValue=value;
+     console.log(value);
+     
+     this.performSearch(value)
+ 
+   }
+
+
+   performSearch(val:string){
+    this.movsService.getSearchMovies(val).subscribe({next:(data)=>{
+      this.filteredMovies=data.results;
+    }})
+
+  }
+
+
+}
