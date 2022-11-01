83ad49fe9529dd03228e3569f72d21fd3f3ebb3a
AmiraAHady <amirasgithub@gmail.com>
2022-11-01 10:21:31 +0200

    adding ui of movie details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MovieslistComponent } from './movieslist/movieslist.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieslistComponent
+    MovieslistComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule
