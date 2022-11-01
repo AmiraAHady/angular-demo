25d5cc7b48935a3c5d9f295b129f60bad19121f9
AmiraAHady <amirasgithub@gmail.com>
2022-11-01 11:43:19 +0200

    ahanges to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MovieslistComponent } from './movieslist/movieslist.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieslistComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule
