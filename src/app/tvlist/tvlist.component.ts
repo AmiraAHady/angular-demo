20f220678b455d0d150eb994fb45138a84032560
AmiraAHady <amirasgithub@gmail.com>
2022-11-02 09:48:08 +0200

    subscribe to movies service in tv component

--- src/app/tvlist/tvlist.component.ts
+++ src/app/tvlist/tvlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-tvlist',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TvlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movsService:MoviesService) { }
 
   ngOnInit(): void {
+    this.movsService.getTVShows().subscribe({next:()=>{}})
   }
+
+
 
 }
